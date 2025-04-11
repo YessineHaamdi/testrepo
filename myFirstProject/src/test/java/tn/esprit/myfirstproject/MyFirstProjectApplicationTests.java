f4c6adfdfa527198d4def0dd797ce15fac03bf13
YessineHaamdi <yessine.hamdi@esprit.tn>
2025-04-11 17:55:18 +0200

    Edited Jenkinsfile

--- myFirstProject/src/test/java/tn/esprit/myfirstproject/MyFirstProjectApplicationTests.java
+++ myFirstProject/src/test/java/tn/esprit/myfirstproject/MyFirstProjectApplicationTests.java
@@ -1,12 +1,10 @@
 package tn.esprit.myfirstproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MyFirstProjectApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
